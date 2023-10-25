23b80d8a8e753b9614f4d1f6465b88831f9ba025
Lady201 <quintanillarojasleydymassiell@gmail.com>
2023-10-24 22:12:46 -0500

    Proyecto casi listo

--- src/main.js
+++ src/main.js
@@ -36,9 +36,7 @@ window.addEventListener("load", () => {
 filterType.addEventListener("change", function () {
   const selectValue = filterType.value;
   allData = filterData(allData, "type", selectValue);
-  console.log("tipo: ", allData);
   if (allData.length > 0) {
-    console.log("contenedor: ", pokemonsContainerRoot);
     const pokemons = renderItems(allData);
     pokemonsContainerRoot.innerHTML = ' '
     pokemonsContainerRoot.appendChild(pokemons)
@@ -52,14 +50,13 @@ filterType.addEventListener("change", function () {
 filterResistant.addEventListener("change", function () {
   const selectValue = filterResistant.value;
   allData = filterData(allData, "resistant", selectValue);
-  console.log("resistant", allData);
 
   if (allData.length > 0) {
     const pokemons = renderItems(allData);
     pokemonsContainerRoot.innerHTML = ' '
     pokemonsContainerRoot.appendChild(pokemons)
   } else {
-    pokemonsContainerRoot.innerHTML = '<p>No hay coincidencias, "click en limpiar búsqueda"</p>';
+    pokemonsContainerRoot.innerHTML = '<p>There is no coincidense, "click on clear search"</p>';
   }
 });
 
@@ -67,7 +64,6 @@ filterResistant.addEventListener("change", function () {
 nameInput.addEventListener("input", function () {
   const byName = nameInput.value;
   allData = filterData(allData, "name", byName);
-  console.log("search", allData);
 
   if (allData.length > 0) {
     const pokemons = renderItems(allData);
@@ -75,7 +71,7 @@ nameInput.addEventListener("input", function () {
     pokemonsContainerRoot.appendChild(pokemons)
   } else {
     pokemonsContainerRoot.innerHTML =
-      '<p>No hay coincidencias, "click en limpiar búsqueda"</p>';
+      '<p>There is no coincidense, "click on clear search"</p>';
   }
 });
 
@@ -83,7 +79,6 @@ nameInput.addEventListener("input", function () {
 selectOrder.addEventListener("change", (event) => {
   const option = event.target.value;
   allData = sortData(allData, "name", option);
-  console.log("order: ", allData);
 
   if (allData.length > 0) {
     const pokemons = renderItems(allData);
@@ -91,12 +86,11 @@ selectOrder.addEventListener("change", (event) => {
     pokemonsContainerRoot.appendChild(pokemons)
   } else {
     pokemonsContainerRoot.innerHTML =
-      '<p>No hay coincidencias, "click en limpiar búsqueda"</p>';
+      '<p>There is no coincidense, "click on clear search"</p>';
   }
 });
 
 limpiarBusqueda.addEventListener("click", function () {
-  console.log('click');
   // Restablece los filtros a su estado predeterminado
   const filterType = document.querySelector(
     "select[data-testid='filter-type']"
@@ -119,5 +113,7 @@ limpiarBusqueda.addEventListener("click", function () {
   // Vuelve a mostrar todos los Pokémon sin filtros
   allData = pokemons;
   pokemonsContainerRoot.innerHTML = ' '
+  // nameInput.value=''
+  // selectOrder.value=''
   pokemonsContainerRoot.appendChild(renderItems(allData))
 });
